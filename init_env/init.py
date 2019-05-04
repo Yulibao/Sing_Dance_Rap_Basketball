b98b92e2ed764bbc99a1a7426582c0073e13cd86
yuliBao <1624689815@qq.com>
2019-05-04 22:02:22 +0800

    文本基本处理

--- init_env/init.py
+++ init_env/init.py
@@ -1,7 +1,11 @@
 import nltk
 
+download_dir = "..\\Data"
+
 if __name__ == '__main__':
-    nltk.download('wordnet', download_dir='..\\Data')
+    nltk.download('punkt', download_dir=download_dir)
+    nltk.download('averaged_perceptron_tagger', download_dir=download_dir)
+    nltk.download('wordnet', download_dir=download_dir)
     """
     使用时导入 先配置数据路径
     from nltk import data
--- model.py
+++ model.py
@@ -1,1 +1,49 @@
-import networkx as nx
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk import pos_tag
+from nltk import data
+
+import networkx
+
+data.path.append('.\\Data')
+
+class Article:
+    text = ''
+    words = []
+
+    def __init__(self, file_name):
+        #with open(file_name,'r') as f:
+            #self.text = f.read().lower()
+        self.text = file_name.lower()
+
+    def tokenize(self):
+        self.words = word_tokenize(self.text)
+
+    def lemmatize(self):
+        lemmatizer = WordNetLemmatizer().lemmatize
+        self.words = [lemmatizer(word) for word in self.words]
+        self.words = [lemmatizer(word,'v') for word in self.words]
+
+    def tag(self):
+        self.words = pos_tag(self.words)
+
+class SemanticNetwork:
+    word_type = ['VB', 'NN']
+    node_word_map = []
+    g = networkx.DiGraph()
+
+    link_threshold = 0.1
+
+    def __init__(self, taged_words, link_threshold):
+        self.link_threshold = link_threshold
+
+        i = 0
+        for word in range(len(taged_words)):
+            if word[1] in self.word_type:
+                self.node_word_map.append((word[0],i))
+                i += 1
+
+        for
+
+    def 
